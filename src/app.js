935c234ec405db69254036a9d105ea227adfe83b
Ciunilius <ciuniliusmasih@gmail.com>
2025-05-04 10:25:16 +0500

    next-app

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const path = require("path");
+const { default: mongoose } = require("mongoose");
+
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+let port = process.env.PORT;
+app.use(express.json());
+app.use(cors());
+const staticPath = path.join(__dirname, "../clined");
+// console.log(staticPath);
+
+app.use(express.static(staticPath));
+
+const ContactSchema = new mongoose.Schema({ name: String, phone: String, email: String, message: String });
+const ContactModel = mongoose.model('contactsStudents', ContactSchema);
+
+// app.get("/", (req, res) => {
+//     res.send("Cliend Comunication on Server")
+// });
+app.post('/contact',(req, res) => {
+    const { name, phone, email, msg } = req.body
+    ContactModel.create({ name, phone, email, message: msg });
+    res.status(201).json({ msg: "Message Send" });
+});
+mongoose.connect(process.env.mongoURI).then(() => {
+    app.listen(port, () => {
+        console.log("I am live to server "+port);
+    });
+}).catch((err) => {
+    console.log('Conntection Faild:' + err);
+});
+// const data = await  ContactModel.create(req.body);
+// const data = await ContactModel.create({ name: name, phone: phone, emai: email, message: message });   
